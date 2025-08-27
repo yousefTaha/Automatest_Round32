482ae6a26e4e94190dec462a09533998c36fb976
Youssef Taha <yousef209@gmail.com>
2025-08-27 16:42:20 +0300

    second task and inhancement
    second task and some inhancement in first task

--- src/test/java/testPackage/FristTask.java
+++ src/test/java/testPackage/FristTask.java
@@ -6,29 +6,15 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FristTask {
-
-
     @Test
-
     public void  test(){
-
-
         WebDriver driver = new ChromeDriver();
         try {
-
         driver.get("https://duckduckgo.com");
-
         String actualTitle = driver.getTitle();
         String expectedTitle = "Google";
-
         Assert.assertEquals(actualTitle, expectedTitle, "Title does not match!");
         } finally {
-
-            driver.quit();
-        }
-
-
-
-
+            driver.quit();}
     }}
 
