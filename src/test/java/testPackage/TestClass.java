b62c553c5c72050e05fea0008776c367f2f63acd
Youssef Taha <yousef209@gmail.com>
2025-09-02 15:19:58 +0300

    update TestClass
    upload fourthTest

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -2,10 +2,13 @@ package testPackage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 import static org.testng.Assert.assertEquals;
 
@@ -28,50 +31,65 @@ public class TestClass {
             Assert.assertTrue(isLogoDisplayed, "Logo is not displayed");
         }
 
-        @Test
-                public void thirdTest() {
-            driver.get("https://duckduckgo.com");
-            By searchInputLocator = By.xpath("//input [@class='searchbox_input__rnFzM']");
-            driver.findElement(searchInputLocator).sendKeys("Selenium WebDriver");
-            driver.findElement(searchInputLocator).submit();
+    /**
+     * open Chrome browser
+     *  open duckduckgo.com
+     *  search for "Selenium WebDriver"
+     *  click on search button
+     *  verify that the first result contains "https://www.selenium.dev/documentation/webdriver/"
+     *  assert that first result is "https://www.selenium.dev/documentation/webdriver/"
+     *  and display message "First search result is match"
+     * */
+    @Test
+    public void thirdTest() {
+        driver.get("https://duckduckgo.com");
+        By searchInputLocator = By.xpath("//input [@class='searchbox_input__rnFzM']");
+        driver.findElement(searchInputLocator).sendKeys("Selenium WebDriver");
+        driver.findElement(searchInputLocator).submit();
+        String firstResultText = "https://www.selenium.dev/documentation/webdriver/";
+        By firstResultLocator = By.xpath("//*[@id=\"r1-0\"]/div[3]/h2/a");
+        String actualFirstResultText = driver.findElement(firstResultLocator).getAttribute("href");
+        assertEquals(actualFirstResultText, firstResultText, "First search result is match");
+    }
 
-            By firstResultLocator = By.xpath("//*[@id=\"r1-0\"]/div[2]/div/div/a");
-            String firstResultText = driver.findElement(firstResultLocator).getText();
-            assertEquals(firstResultText, "https://www.selenium.dev/documentation/webdriver/", "First search result does");
-            //Assert.assertTrue (firstResultLocator = By.xpath("//*[@id=\"r1-0\"]/div[2]/div/div/a/div/p/span[1]");
-          //  (firstResultLocator=("https://www.selenium.dev/documentation/webdriver/"), "", "Search results page title is match");
+    /**
+     * Open Mozilla Firefox
+     * navigate to duckduckgo.com
+     * search for "TestNG"
+     * Assert that the text of the fourth result is [TestNG Tutorial]
+     * Close Mozilla Firefox
+     */
 
-            //to see this code later
-            /*
-               @Test
-    public void taskProblemThree(){
-        SoftAssert softAssert = new SoftAssert();
-        By searchBar = By.id("searchbox_input");
-        By searchBtn = By.xpath("//button[@aria-label='Search']");
-        By firstResult = By.xpath("//span[text()='WebDriver - Selenium']");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-       WebElement waitedBar = wait.until(ExpectedConditions.visibilityOfElementLocated(searchBar));
-       // a jump over to the assertion then to the quit. (solved when each waitedElement is before it's interaction
+    @Test
+    public void fourthTest() {
+        driver.get("https://duckduckgo.com");
+        By searchInputLocator = By.xpath("//input [@class='searchbox_input__rnFzM']");
+        driver.findElement(searchInputLocator).sendKeys("TestNG");
+        driver.findElement(searchInputLocator).submit();
+        String fourthResultText = "TestNG Tutorial";
+        By fourthResultLocator = By.xpath("//*[@id=\"r1-3\"]/div[3]/h2/a");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        String actualFourthResultText = driver.findElement(fourthResultLocator).getText();
+        assertEquals(actualFourthResultText, fourthResultText, "Fourth search result is match");
 
-       waitedBar.sendKeys("Selenium WebDriver");
-        WebElement waitedSearchBtn =  wait.until(ExpectedConditions.presenceOfElementLocated(searchBtn));
-       waitedSearchBtn.click();
-        WebElement waitedResult = wait.until(ExpectedConditions.presenceOfElementLocated(firstResult));
-       waitedResult.click();
 
-        String URL = driver.getCurrentUrl();
-        softAssert.assertEquals(URL, "https://www.selenium.dev/documentation/webdriver/");
-        softAssert.assertAll();
-             */
+
+
 
 
         }
 
 
+
+
+
+
+
+
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
-//        driver = new FirefoxDriver();
+        driver = new FirefoxDriver();
     }
     @AfterMethod
     public void tearDown() {
