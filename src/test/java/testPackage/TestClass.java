2b49815fcf947300ac575207bda8813dc7b8e2e3
Youssef Taha <yousef209@gmail.com>
2025-09-02 11:19:29 +0300

    update

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -7,6 +7,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class TestClass {
     private WebDriver driver;
 
@@ -15,7 +17,7 @@ public class TestClass {
             driver.get("https://duckduckgo.com");
             String actualTitle = driver.getTitle();
             String expectedTitle = "Google";
-            Assert.assertEquals(actualTitle, expectedTitle, "Title mismatch");
+            assertEquals(actualTitle, expectedTitle, "Title mismatch");
         }
 
         @Test
@@ -33,7 +35,34 @@ public class TestClass {
             driver.findElement(searchInputLocator).sendKeys("Selenium WebDriver");
             driver.findElement(searchInputLocator).submit();
 
+            By firstResultLocator = By.xpath("//*[@id=\"r1-0\"]/div[2]/div/div/a");
+            String firstResultText = driver.findElement(firstResultLocator).getText();
+            assertEquals(firstResultText, "https://www.selenium.dev/documentation/webdriver/", "First search result does");
+            //Assert.assertTrue (firstResultLocator = By.xpath("//*[@id=\"r1-0\"]/div[2]/div/div/a/div/p/span[1]");
+          //  (firstResultLocator=("https://www.selenium.dev/documentation/webdriver/"), "", "Search results page title is match");
 
+            //to see this code later
+            /*
+               @Test
+    public void taskProblemThree(){
+        SoftAssert softAssert = new SoftAssert();
+        By searchBar = By.id("searchbox_input");
+        By searchBtn = By.xpath("//button[@aria-label='Search']");
+        By firstResult = By.xpath("//span[text()='WebDriver - Selenium']");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+       WebElement waitedBar = wait.until(ExpectedConditions.visibilityOfElementLocated(searchBar));
+       // a jump over to the assertion then to the quit. (solved when each waitedElement is before it's interaction
+
+       waitedBar.sendKeys("Selenium WebDriver");
+        WebElement waitedSearchBtn =  wait.until(ExpectedConditions.presenceOfElementLocated(searchBtn));
+       waitedSearchBtn.click();
+        WebElement waitedResult = wait.until(ExpectedConditions.presenceOfElementLocated(firstResult));
+       waitedResult.click();
+
+        String URL = driver.getCurrentUrl();
+        softAssert.assertEquals(URL, "https://www.selenium.dev/documentation/webdriver/");
+        softAssert.assertAll();
+             */
 
 
         }
